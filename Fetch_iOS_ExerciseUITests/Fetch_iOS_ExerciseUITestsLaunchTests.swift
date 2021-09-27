e8b466037fef49f69bd40dee1899efe44cd0c764
thebmw37 <brycewoods37@gmail.com>
2021-09-27 14:49:04 -0600

    Initial Commit

---
+++ Fetch_iOS_ExerciseUITests/Fetch_iOS_ExerciseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fetch_iOS_ExerciseUITestsLaunchTests.swift
+//  Fetch_iOS_ExerciseUITests
+//
+//  Created by Bryce Woods on 9/27/21.
+//
+
+import XCTest
+
+class Fetch_iOS_ExerciseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
